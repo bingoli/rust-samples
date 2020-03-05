c5b2253543e632fdaac5e12ccc32eefabe062a97
bingoli <bingoli2014@qq.com>
2020-03-05 19:14:45 +0800

    修改tokio的版本为0.2.23
    Change-Id: I06cc6cdbe7884f989c4392fc40c97e294083d4bf

--- tokio_perf/src/bin/main.rs
+++ tokio_perf/src/bin/main.rs
@@ -1,63 +1,17 @@
-extern crate tokio;
 extern crate chrono;
-#[macro_use]
-extern crate lazy_static;
+extern crate tokio;
 
-use std::sync::RwLock;
-use std::thread;
+use chrono::prelude::Utc;
 use std::time::Duration;
+use tokio::time;
 
-use tokio::{prelude::*, runtime, timer::Interval};
-use chrono::prelude::*;
-
-lazy_static! {
-    static ref MY_RUNTIME: runtime::Runtime = runtime::Builder::new()
-        .core_threads(1)
-        .blocking_threads(10)
-        .keep_alive(Some(Duration::from_secs(60)))
-        .name_prefix("tokio-thread")
-        .after_start(|| {
-        })
-        .before_stop(|| {
-        })
-        .build()
-        .expect("Initialize runtime failed");
-
-    static ref TIME_COUNT: RwLock<i32> = {
-        RwLock::new(0)
-    };
-}
-
-
-fn test_interval() {
-    let interval = Duration::from_millis(1000);
-    let task = Interval::new_interval(interval)
-        .for_each(move |_| {
-
-            let count = {
-                let mut w = TIME_COUNT.write().unwrap();
-                *w += 1;
-                w
-            };
-            let my_time = Utc::now();
-            println!("{}: {}", count, my_time.format("%T"));
-            Ok(())
-        })
-        .then(|ret| {
-            if let Err(e) = ret {
-                println!("run task failed: {:?}", e);
-            }
-            Ok(())
-        });
-    
-        MY_RUNTIME.executor().spawn(task);
-}
-
-fn main() {
-    test_interval();
-    
+#[tokio::main]
+async fn main() {
+    let mut interval = time::interval(Duration::from_millis(1000));
+    let mut count: i32 = 0;
     loop {
-        let sleep_duration = Duration::from_secs(60*60*1000);
-        thread::sleep(sleep_duration);
+        count += 1;
+        let time = interval.tick().await;
+        println!("{}, {}, {:?}", count, Utc::now().format("%T"), time);
     };
 }
